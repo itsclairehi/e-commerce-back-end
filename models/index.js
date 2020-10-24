bcc7732b907d537a6b8ea0261277380cd55d7eb5
claire p <cpuckett9@gmail.com>
2020-10-24 16:02:50 -0600

    completed associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //as: '', if there's a new name for attribute
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  //as:'',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
@@ -18,3 +31,16 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+// User.belongsToMany(Post, {
+//   through: Vote,
+//   as: 'voted_posts',
+//   foreignKey: 'user_id'
+// });
+
+// Post.belongsToMany(User, {
+//   through: Vote,
+//   as: 'voted_posts',
+//   foreignKey: 'post_id'
+// });
