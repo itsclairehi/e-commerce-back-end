36d592034bad4bfdf164aa1fe655c5b0dfe2b3a6
claire p <cpuckett9@gmail.com>
2020-10-23 21:41:24 -0600

    updated model columns

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,42 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        //!!validate that price is a decimal
+      },
+    },
+    stocke: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // !!set a default value of 10,
+      validate: {
+        //!!validate that value is numeric
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        //reference category model's id
+      }
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,24 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        //!! references product model id
+      }
+    },
+    tag_id: {
+      type:DataTypes.INTEGER,
+      references: {
+        //!! referenes tag model's id
+      }
+    }
   },
   {
     sequelize,
